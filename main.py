122f7bdeab0908b7610c8b836e9b4dfee12f33be
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-11-09 19:37:58 -0600

    main

---
+++ main.py
@@ -0,0 +1,39 @@
+#Universidad del Valle de Guatemala
+#Juan Pablo Pineda
+#Hugo Román
+#Laurelinda Gómez
+#8/11/2021
+#Lab 10
+
+def menu():
+    
+    print("Bienvenido, selecciona una opcion")
+    print ("\ 1. - Generar claves ")
+    print ("\ 2. - Encriptar ")
+    print ("\ 3. - Decriptar ")
+    print ("\ 4. - Salir ")
+    
+while True:
+    menu()
+    
+    opcion = input(" Escriba una de las opciones ")
+    
+    if opcion == "1":
+        
+    elif opcion == "2":
+        encrip = input("Ingrese el mensaje que desea encriptar: ")
+        
+    
+    elif opcion == "3":
+        decrip = input("Ingrese el mensaje que desea decriptar: ")
+        
+    elif opcion == "4":
+        print("Gracias por utilizar el programa")
+    
+    else:
+        print("Opcion incorrecta")
+        
+        
+        
+    
+    
